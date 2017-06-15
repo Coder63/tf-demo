a951323dd45d990153e99d92b3c00f07477e4b23
Coder63 <pbharath63@gmail.com>
2017-06-15 13:41:49 -0700

    Create main.tf

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ provider "aws" {
 
 resource "aws_instance" "example" {
   # Ubuntu Server 14.04 LTS (HVM), SSD Volume Type in us-east-1
-  ami = "ami-efd0428f"
+  ami = "${var.ami_name}"
   instance_type = "t2.micro"
   vpc_security_group_ids = ["${aws_security_group.instance.id}"]
 
